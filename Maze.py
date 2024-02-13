9ce041391b4e9d96f0abc7655d05e05ccb24fa4e
Kyle Woolford <kyle.woolford@gmail.com>
2024-02-13 11:39:05 -0500

    Updated code to use more readable variable names

--- Maze.py
+++ Maze.py
@@ -1,39 +1,52 @@
-import random, pygame, numpy as np, sys
+import random, pygame, numpy as np
 
+# Maze Dimensions
+maze_width, maze_height = 20,20
+
+# Screen Dimensions
+width, height = 1000, 1000
+
+# Calculating Cell Size
+cell_width = width // maze_width
+cell_height = height // maze_height
 
 pygame.init()
 class HERO:
-    self.body = np.array([self.x, self.y])
     def __init__(self):
-        self.body = []
-    
+        self.body = (10,10)
+
+    def draw_hero(self):
+        hero_rect = pygame.Rect(self.body[0]*cell_width, self.body[1]*cell_height, cell_width, cell_height)
+        pygame.draw.rect(screen, (255, 0, 0), hero_rect)
+
 class SPIKE:
     def __init__(self):
-        self.x = random.randint(1,cell_number)
-        self.y = random.randint(1,cell_number)
-        self.pos = np.array([self.x, self.y])
+        self.x = random.randint(1,maze_width)
+        self.y = random.randint(1,maze_height)
+        self.pos = self.x, self.y
         # Create an x and y position
         # Draw a triangle
     def draw_spike(self):
-        spike_rect = pygame.Rect(self.pos[0]*cell_size,self.pos[1]*cell_size,cell_size,cell_size)
+        spike_rect = pygame.Rect(self.pos[0]*cell_width,self.pos[1]*cell_height,cell_width,cell_height)
         pygame.draw.rect(screen, (1,1,1), spike_rect)
 
-cell_size = 30
-cell_number = 20
 
-screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
+
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 spike = SPIKE()
+hero = HERO()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-            sys.exit()
+  
     screen.fill((75,75,75))
     spike.draw_spike()
+    hero.draw_hero()
     # Point specified is the top left of the surface
     pygame.display.update()
     clock.tick(60)
