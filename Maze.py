f7e0eeb35e5a8e4edd66f17c3b605150d030bbd1
Kyle.Woolford <Kyle.woolford@gmail.com>
2024-02-13 23:15:04 -0500

    Testing a commit

--- Maze.py
+++ Maze.py
@@ -18,6 +18,16 @@ class HERO:
     def draw_hero(self):
         hero_rect = pygame.Rect(self.body[0]*cell_width, self.body[1]*cell_height, cell_width, cell_height)
         pygame.draw.rect(screen, (255, 0, 0), hero_rect)
+    
+class MAZE:
+    def __init__(self):
+        self.maze = np.zeros((maze_width, maze_height))
+        self.maze[0] = 1
+        self.maze[-1] = 1
+        self.maze[:,0] = 1
+        self.maze[:,-1] = 1
+        print(self.maze)
+        # Adding a comment for a test
 
 class SPIKE:
     def __init__(self):
@@ -26,6 +36,8 @@ class SPIKE:
         self.pos = self.x, self.y
         # Create an x and y position
         # Draw a triangle
+
+
     def draw_spike(self):
         spike_rect = pygame.Rect(self.pos[0]*cell_width,self.pos[1]*cell_height,cell_width,cell_height)
         pygame.draw.rect(screen, (1,1,1), spike_rect)
@@ -37,6 +49,7 @@ clock = pygame.time.Clock()
 
 spike = SPIKE()
 hero = HERO()
+maze = MAZE()
 running = True
 while running:
     for event in pygame.event.get():
