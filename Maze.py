80eb770830f0f613882b4dd0d8ca911bf6016757
Kyle Woolford <kyle.woolford@gmail.com>
2024-02-15 10:47:08 -0500

    Progressing. Added walls to numpy array and spikes do not generate near walls

--- Maze.py
+++ Maze.py
@@ -10,33 +10,37 @@ width, height = 1000, 1000
 cell_width = width // maze_width
 cell_height = height // maze_height
 
-pygame.init()
-class HERO:
-    def __init__(self):
-        self.body = (10,10)
-
-    def draw_hero(self):
-        hero_rect = pygame.Rect(self.body[0]*cell_width, self.body[1]*cell_height, cell_width, cell_height)
-        pygame.draw.rect(screen, (255, 0, 0), hero_rect)
-
+pygame.init()    
 class SPIKE:
     def __init__(self):
-        self.x = random.randint(1,maze_width)
-        self.y = random.randint(1,maze_height)
+        self.x = random.randint(2, maze_width - 3)
+        self.y = random.randint(2, maze_height - 3)
         self.pos = self.x, self.y
         # Create an x and y position
         # Draw a triangle
+        
     def draw_spike(self):
         spike_rect = pygame.Rect(self.pos[0]*cell_width,self.pos[1]*cell_height,cell_width,cell_height)
         pygame.draw.rect(screen, (1,1,1), spike_rect)
 
 
+class MAZE:
+    def __init__(self):
+        self.maze = np.zeros((maze_width, maze_height), dtype=int)
+
+        # Add walls to the maze
+        self.maze[0,:] = 1
+        self.maze[:,0] = 1
+        self.maze[-1,:] = 1
+        self.maze[:,-1] = 1
+        print(self.maze)
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 spike = SPIKE()
-hero = HERO()
+maze1 = MAZE()
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -46,7 +50,6 @@ while running:
   
     screen.fill((75,75,75))
     spike.draw_spike()
-    hero.draw_hero()
     # Point specified is the top left of the surface
     pygame.display.update()
     clock.tick(60)
